4826aac818209bb1de0e332729d384ad07da05ca
zxl <zxl-jacob@outlook.com>
2017-10-23 10:25:19 +0800

    Revert "Revert "repeat.js、pipeLine.js""
    This reverts commit 8e3200cbc4caef83579be2f9324cbdb71c8a2ab2.

---
+++ repeat.js
@@ -0,0 +1,23 @@
+
+
+const repeat = function (list, result = [], count) {
+  if (list.length === 0) {
+    return result;
+  }
+  // 每组的请求个数
+  let send = list.splice(0, count);
+  return Promise.all(send.map((i) => {
+    return httpRequest(i);
+  })).then(function (fetchResult) {
+    // 如果组内请求只要有一个错误就从新再拼接到请求列表
+    if (fetchResult[0].code != 0 || fetchResult[1].code != 0) {
+      list.concat(send);
+    } else {
+      fetchResult = fetchResult.map(function (item) {
+        return item.result.data
+      })
+      result = result.concat(fetchResult)
+    }
+    return repeat(list, result, count)
+  })
+}
